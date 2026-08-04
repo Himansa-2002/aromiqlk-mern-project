baf70943eb59331c2da9cdd59b63fd872617a4d5
Dilmi Chamya <dilmichamya48@gmail.com>
2026-08-04 16:09:34 +0530

    feat: implement application routing with site and admin layouts in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ import Checkout from "./pages/customer/CheckoutPage.jsx";
 import OrderSuccess from "./pages/customer/OrderSuccessPage.jsx";
 import Orders from "./pages/customer/OrdersPage.jsx";
 import OrderDetails from "./pages/customer/OrderDetailsPage.jsx";
-import AdminDashboardPage from "./pages/admin/AdminDashboardPage.jsx";
+//import AdminDashboardPage from "./pages/admin/AdminDashboardPage.jsx";
 import AdminOrders from "./pages/admin/AdminOrdersPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
@@ -63,7 +63,7 @@ function SiteLayout() {
         <Route path="/order-success" element={<OrderSuccess />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/order-details" element={<OrderDetails />} />
-        <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/admin-orders" element={<AdminOrders />} />
         <Route path="*" element={<NotFoundPage />} />
 
